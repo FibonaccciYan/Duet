af07c722b6d5d508b071020599328704064182c3
BaideBear <3345180277@qq.com>
2026-09-16 19:17:59 +0800

    merge(scripts): restore dense evaluation utilities

---
+++ scripts/chain_c1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Card 1 (GPU 0): SDAR MMLU (~8h)
+set -u
+DRIVER=/data0/gs/SparseDLM_LLaDA_SDAR/scripts/driver_eval_dense.sh
+env HF_DATASETS_CACHE=/data0/gs/.hf_cache_sdar_dense_b32_thr095 \
+  BENCHMARKS="mmlu" "$DRIVER" sdar retest_sdar 0 12338 \
+  || { echo "card1 SDAR mmlu FAILED"; exit 1; }
+echo "CARD 1 DONE"
